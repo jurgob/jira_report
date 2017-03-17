446890d79dec286a77c09ad47a3ae3eacf5234ae
Jurgo Boemo <jurgo.boemo@gmail.com>
2017-03-17 23:03:25 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+var JiraApi = require('jira-client');
+
+
+var jira = new JiraApi({
+  protocol: 'https',
+  host: 'nexmoinc.atlassian.net',
+  username: process.env.JIRA_USER,
+  password: process.env.JIRA_PASS,
+  apiVersion: '2',
+  strictSSL: true
+});
+
+var jqlQuery = "assignee = currentUser() and Sprint in  openSprints() and status = closed"
+jira.searchJira(jqlQuery)
+  .then(resp => {
+    const res = resp.issues
+      .map(t => ({key:t.key, summary: t.fields.summary   }))
+      .forEach(t => console.log(`${t.key} - ${t.summary}`   ))
+  })
+  .catch(err => {
+    console.error(err);
+  });
